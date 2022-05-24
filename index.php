ef2a820604317da4ad1d642c9271afb0d5cb5ef9
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-05-24 14:01:12 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FilmeGratis</title>
+    <link rel="shortcut icon " href="frontend/assets/images/icon.ico" type="x-icon">
+</head>
+<body>
+    <section class="site-container">
+            <header class="site-header">
+                <div class="brand-container">
+                    
+                </div>
+            </header>
+
+    </section>
+</body>
+</html>
