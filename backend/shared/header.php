4ca9e1da3b99415388a8dc1c97525ab5fc92db32
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-05-25 03:00:06 -0700

    starting signup page and style it

---
+++ backend/shared/header.php
@@ -0,0 +1,18 @@
+
+<?php  
+        if(!$pageTitle){
+            $pageTitle="Netflix ";
+        }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="shortcut icon " href="frontend/assets/images/Netflix.ico" type="x-icon">
+    <link rel="stylesheet" href="frontend/assets/css/master.css">
+</head>
+<body>
